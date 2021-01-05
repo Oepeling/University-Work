cd3649383103fa14a80260e256b4cc1582fcbfb2
Livia <livia.magureanu@gmail.com>
2021-01-05 20:34:23 +0200

    Clean up

--- Optional-Django/project/notes/models.py
+++ Optional-Django/project/notes/models.py
@@ -40,11 +40,6 @@ class ToDoItem(models.Model):
     )
 
 
-def user_directory_path(instance, filename):
-    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return 'user_{0}/{1}'.format(instance.user.id, filename)
-
-
 class Note(BasicNote):
     text = models.TextField(
         'Text'
