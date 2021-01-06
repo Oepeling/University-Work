e864d5b0f4a7c340e7a780a30e4f76a4408c46d6
Livia <livia.magureanu@gmail.com>
2021-01-06 03:04:15 +0200

    Login / Logout redirect

--- Optional-Django/project/notes/urls.py
+++ Optional-Django/project/notes/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+import django.contrib.auth.views as auth_views
 
 from . import views
 
@@ -7,6 +8,7 @@ urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('todos/', views.ToDoListIndexView.as_view(), name='todo_lists'),
     path('notes/', views.NoteIndexView.as_view(), name='notes'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='notes:home_page')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/register/', views.register_user, name='register'),
 ]
